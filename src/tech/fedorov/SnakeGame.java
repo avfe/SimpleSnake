a9472a69c03630814490db266a265ea47fef03aa
avfe <sen4ka@mail.ru>
2020-11-01 21:20:52 +0300

    Update!

--- src/tech/fedorov/SnakeGame.java
+++ src/tech/fedorov/SnakeGame.java
@@ -1,8 +1,10 @@
 package tech.fedorov;
 
-public class Main {
+public class SnakeGame {
 
     public static void main(String[] args) {
-	// write your code here
+
+        new GameFrame();
+
     }
 }
